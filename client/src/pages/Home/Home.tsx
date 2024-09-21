272d184cb48a87f093bea298173b4e2f4fa6df00
Clay G <buzzscoutbark@gmail.com>
2024-09-21 13:27:40 -0500

    homepage changes

--- client/src/pages/Home/Home.tsx
+++ client/src/pages/Home/Home.tsx
@@ -1,7 +1,12 @@
+import "./Home.css"
+
 function Home()
 {
     return <div>
-        <h1>This is the home page.</h1>
+        <div>
+            <h1>Moti-Vibes</h1>
+        </div>
+        <h4>Crowdsourced Academic Encouragement to Combat Procrastination.</h4>
     </div>
 }
 
