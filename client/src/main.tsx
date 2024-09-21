936cf32cfb6c2584e5c407df3b16b73936846ba1
SlinkyShelf <77686856+SlinkyShelf@users.noreply.github.com>
2024-09-21 00:57:19 -0500

    Firebase updates, allowing for an api key

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -5,6 +5,8 @@ import './index.css'
 import Home from './pages/Home/Home.tsx'
 import Userinfo from './pages/Userinfo/Userinfo.tsx'
 
+import { app, auth } from './modules/firebase.ts'
+
 import {
   createBrowserRouter,
   RouterProvider,
@@ -23,6 +25,7 @@ const router = createBrowserRouter([
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
+    {auth.currentUser != null?auth.currentUser.displayName:"no user"}
     <RouterProvider router={router} />
   </StrictMode>,
 )
