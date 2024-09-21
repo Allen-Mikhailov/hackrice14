62f328813b14e74adb261f2d4e6c2e656118cbba
Justin Lopato <justin@lopato.org>
2024-09-21 18:18:03 -0500

    updated profile router

--- server/src/routers/profile.ts
+++ server/src/routers/profile.ts
@@ -1,6 +1,8 @@
 import { Router, Response } from "express";
 import { authMiddleware, UserData } from "../middleware/auth";
+import { database } from "../mongodb";
 
+const users = database.collection("users");
 const profile = Router();
 
 profile.use(authMiddleware);
@@ -9,8 +11,15 @@ profile.get("/me", (_req, res: Response<{}, { user: UserData }>) => {
   res.json(res.locals.user);
 });
 
-profile.get("/:id", (_req, res: Response<{}, { user: UserData }>) => {
-  res.json(res.locals.user);
+profile.get("/:id", async (req, res: Response<{}, { user: UserData }>) => {
+  let user;
+  
+  if (!res.locals.user.matches.includes(req.params.id) || (user = await users.findOne<UserData>({ "id": req.params.id })) === null) {
+    res.status(404).send("Chat not found");
+    return;
+  }
+
+  res.json(user);
 });
 
 export default profile;
