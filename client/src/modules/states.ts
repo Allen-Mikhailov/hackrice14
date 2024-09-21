e5f0693dd2b21e5261448ecc03221878cba5cbac
SlinkyShelf <77686856+SlinkyShelf@users.noreply.github.com>
2024-09-21 16:48:26 -0500

    Fixed path on the server package

--- client/src/modules/states.ts
+++ client/src/modules/states.ts
@@ -1,6 +1,6 @@
 import { createState } from "state-pool";
 // import { User } from "firebase/auth";
-import { UserData } from "/server/src/middleware/auth.ts";
+import { UserData } from "server/src/middleware/auth";
 
 const user_data_state = createState<UserData | null>(null)
 
