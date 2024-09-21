7872a2113a202e961099ccb7df015bc690ddc7a8
Clay G <buzzscoutbark@gmail.com>
2024-09-20 23:59:09 -0500

    added webpages

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -3,8 +3,22 @@ import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 
+import Home from './pages/Home/Home.tsx'
+
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home/>,
+  },
+]);
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
