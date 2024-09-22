8c0c3e8f1919b75b2ab8151bdc4094289f079b17
SlinkyShelf <77686856+SlinkyShelf@users.noreply.github.com>
2024-09-21 19:07:10 -0500

    Improved navbar

--- client/src/pages/Navbar/Navbar.tsx
+++ client/src/pages/Navbar/Navbar.tsx
@@ -1,6 +1,7 @@
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
 import './Navbar.css'
 import React, { useState, useEffect } from 'react';
 import { User } from 'firebase/auth';
@@ -11,12 +12,6 @@ import {  } from '../../modules/states';
 
 import { Outlet, Link } from "react-router-dom";
 import { auth, google_auth, signInWithPopup, signOut } from '../../modules/firebase';
-
-import Home from '../Home/Home'
-import Userinfo from '../Userinfo/Userinfo'
-import Catalog from '../Catalog/Catalog'
-import Chat from '../Chat/Chat'
-import ChatSelect from '../ChatSelect/ChatSelect'
 
 
 
@@ -56,7 +51,7 @@ function SignedIn()
           
           <Button onClick={SignOut} type="submit">Login Out</Button>
         </Col>
-        <div style={{width: "50px"}}></div>
+        {/* <div style={{width: "50px"}}></div> */}
       </Row>
     </Form>
   </>)
@@ -64,14 +59,7 @@ function SignedIn()
 
 function SignedOut()
 {
-  return <Form>
-  <Row>
-    <Col xs="auto">
-      <Button onClick={SignIn} type="submit">Login/Signup</Button>
-    </Col>
-    <div style={{width: "50px"}}></div>
-  </Row>
-</Form>
+  return <Button onClick={SignIn} type="submit">Login/Signup</Button>
 }
 
 function BaseNavbar() {
@@ -86,11 +74,7 @@ function BaseNavbar() {
 
   return (
     <>
-      <div>
-      
-      </div>
-      <BrowserRouter>
-      <Navbar bg="dark" data-bs-theme="dark">
+      {/* <Navbar bg="dark" data-bs-theme="dark">
           <div style={{width: "50px"}}></div>
           <Navbar.Brand href="/">Moti-Vibes</Navbar.Brand>
           <Nav className="me-auto">
@@ -100,22 +84,27 @@ function BaseNavbar() {
             <Nav.Link as={Link} to="chatSelect">Chat</Nav.Link>
           </Nav>
           {user?<SignedIn/>:<SignedOut/>}
-          
-      </Navbar>
-      <div style={{padding: 50}}>
-        <Routes>
-          <Route path="/" element={<Home/>} />
-          <Route path="/userinfo" element={<Userinfo/>} />
-          <Route path="/catalog" element={<Catalog/>} />
-          <Route path="/chat" element={<Chat/>} />
-          <Route path="/chatSelect" element={<ChatSelect/>} />
-        </Routes>
-      </div>
-      </BrowserRouter>
-      <br />
+      </Navbar> */}
+      <Navbar collapseOnSelect expand="lg" className="bg-body-tertiary" bg="dark" data-bs-theme="dark">
+      <Container>
+        <Navbar.Brand href="#home">Moti-Vibes</Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link as={Link} to="userinfo">User Profile</Nav.Link>
+            <Nav.Link as={Link} to="catalog">Find Unmotivated</Nav.Link>
+            <Nav.Link as={Link} to="chat">Test chat(will eventually be in chatselect)</Nav.Link>
+            <Nav.Link as={Link} to="chatSelect">Chat</Nav.Link>
+          </Nav>
+          {user?<SignedIn/>:<SignedOut/>}
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+      
+      {/* <br />
       <div id="detail">
         <Outlet />
-      </div>
+      </div> */}
     </>
   );
 }
