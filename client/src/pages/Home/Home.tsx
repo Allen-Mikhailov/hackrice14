d9a471571a6429ffff74600fdfdff0e44df65696
kevinbaojz <kevinbaojz@gmail.com>
2024-09-22 09:20:11 -0500

    white

--- client/src/pages/Home/Home.tsx
+++ client/src/pages/Home/Home.tsx
@@ -18,13 +18,15 @@ function Home()
     <br></br>
     <br></br>
     <br></br>
-    Designed 
+    <span style={{color: "white"}}>Designed</span> 
     <span style={{color: "#8271ff"}}> <strong>for college students, by college students.</strong>  </span> 
     
 
     <br></br>
     With a matching algorithm powered by
     <span style={{color:"#FCE7A2"}}> <strong>generative AI, </strong> </span>
+    <span style={{color: "white"}}></span>With a matching algorithm designed to get you in touch with
+    <span style={{color:"#FCE7A2"}}> <strong>people to cheer you on, </strong> </span>
     Moti-Vibes helps you 
     find motivation to finish that pesky assignment.
 
