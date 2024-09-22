d7f9c8272de5a65766356247b9db977963c158c8
Clay G <buzzscoutbark@gmail.com>
2024-09-21 21:15:05 -0500

    style

--- client/src/pages/Home/Home.tsx
+++ client/src/pages/Home/Home.tsx
@@ -3,10 +3,21 @@ import "./Home.css"
 function Home()
 {
     return <div>
-        <div>
-            <h1>Moti-Vibes.</h1>
-        </div>
+        <div className="cool-title">
+            Moti-Vibes.
+            </div>
         <h4>Crowdsourced Academic Encouragement to Combat Procrastination.</h4>
+    <br></br>
+    Designed 
+    <span style={{color: "green"}}> for college students, </span> 
+    by college students. 
+
+    <br></br>
+    With a matching algorithm powered by Generative AI, 
+    Moti-Vibes 
+    helps you 
+    be held accountable for your terrible work ethic.
+
     </div>
     
 }
